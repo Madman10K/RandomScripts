4a2fcff7bfc9a4f0c937fc74306836bce7a03cdf
Stanislav Vasilev <stanv.contact176@gmail.com>
2021-07-21 18:24:44 +0300

    Update Weather.py

--- Python/Weather.py
+++ Python/Weather.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-# Returns a weather report with a corresponding emoji in the terminal from openweathermap
 import requests
 
-CITY = YOUR_CITY_NUMBER
-API_KEY = "YOUR_API_KEY"
+CITY = 729114
+API_KEY = "bb62461c8c7e74cb3ae36bf4b29a34f1"
 UNITS = "Metric"
 UNIT_KEY = "C"
 
@@ -12,6 +11,8 @@ try:
     if REQ.status_code == 200:
         CURRENT = REQ.json()["weather"][0]["description"].capitalize()
         TEMP = int(float(REQ.json()["main"]["temp"]))
+        HUMIDITY = int(float(REQ.json()["main"]["humidity"]))
+        WIND = float(REQ.json()["wind"]["speed"])
 
         WEATHER = "☁️ "
         if CURRENT == "Clear sky":
@@ -24,7 +25,7 @@ try:
             WEATHER = "☃️ "
         elif CURRENT == "Light rain" or CURRENT == "Moderate rain" or CURRENT == "Heavy intensity rain" or CURRENT == "Very heavy rain" or CURRENT == "Extreme rain" or CURRENT == "Freezing rain" or CURRENT == "Light intensity shower rain" or CURRENT == "Shower rain" or CURRENT == "Heavy intensity shower rain" or CURRENT == "Ragged shower rain" or CURRENT == "Light intensity drizzle" or CURRENT == "Drizzle" or CURRENT == "Heavy intensity drizzle" or CURRENT == "Light intensity drizzle rain" or CURRENT == "Drizzle rain" or CURRENT == "Heavy intensity drizzle rain" or CURRENT == "Shower rain and drizzle" or CURRENT == "Heavy shower rain and drizzle" or CURRENT == "Shower drizzle" or CURRENT == "Thunderstorm with light rain" or CURRENT == "Thunderstorm with rain" or CURRENT == "Thunderstorm with heavy rain" or CURRENT == "Light thunderstorm" or CURRENT == "Thunderstorm" or CURRENT == "Heavy thunderstorm" or CURRENT == "Ragged thunderstorm" or CURRENT == "Thunderstorm with light drizzle" or CURRENT == "Thunderstorm with drizzle" or CURRENT == "Thunderstorm with heavy drizzle":
             WEATHER = "⛈️ "
-        print("{}{}°{}".format(WEATHER, TEMP, UNIT_KEY))
+        print("{}°{}{}, 💨{}m/s, 💧{}%".format(WEATHER, TEMP, UNIT_KEY, WIND, HUMIDITY))
     else:
         print("Error code: " + str(REQ.status_code))
 except (ValueError, IOError):
